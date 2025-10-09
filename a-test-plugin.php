32cfce1f829a7e2f924604ab120d42bb971f1e87
Marco Almeida <marcoalmeida@webdados.pt>
2025-10-09 14:59:31 +0100

    Debug FTP

--- a-test-plugin.php
+++ a-test-plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:          A Test Plugin
  * Description:          A test plugin for GitHub actions testing
- * Version:              0.0.3
+ * Version:              0.0.4
  * Author:               Naked Cat Plugins (by Webdados)
  * Author URI:           https://nakedcatplugins.com
  * Text Domain:          a-test-plugin
