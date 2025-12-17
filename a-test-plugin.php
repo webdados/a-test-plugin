4d29f083c9f816408004ecb185ae16c449aad8d5
Marco Almeida <marcoalmeida@webdados.pt>
2025-12-17 10:03:41 +0000

    0.2

--- a-test-plugin.php
+++ a-test-plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:          A Test Plugin
  * Description:          A test plugin for GitHub actions testing
- * Version:              0.1
+ * Version:              0.2
  * Author:               Naked Cat Plugins (by Webdados)
  * Author URI:           https://nakedcatplugins.com
  * Text Domain:          a-test-plugin
